b17922395279cbde3e60223ca6b5280a0c2c59e4
Kelson Teixeira <txkelson@gmail.com>
2018-11-27 10:50:04 -0200

    new sort format and responsible design

--- my_sites/Options_sort/js/script.js
+++ my_sites/Options_sort/js/script.js
@@ -1,10 +1,15 @@
-//variables
+
+
+//------------------variables-------------
 var options = [];
+
+var more = [];
 
 var verify = 0;
 
 
-//functions
+
+//------------------functions----------------
 
 function add() {
     var op = document.forms["get_option"]["new_option"].value;
@@ -15,22 +20,33 @@ function add() {
 }
 
 function sort (){
-    var opp = options.length + 1;
-    var rnd = Math.floor(Math.random() * opp);
-    rnd = rnd - 1;
+    var rnd = Math.floor(Math.random() * options.length);
     return rnd;
 }
 
 function show (rnd){
-    $("h5").text(options[rnd]);
+    more = [];
+    if (options.length == 0){
+        $("h5").text("Sem Opção");
+    }else{
+        $("h5").text(options[rnd]);
+    }
+    for (var i = 0; i < options.length; i++){
+        if (options[i] != options[rnd]){
+            more.push(options[i]);
+        }
+    }
+    options = more;
+    $(".the_list").text(options);
     $("h5").css("display", "block");
     $("h4").css("display", "block");
     $(".button3").css("display", "block");
+    $(".button4").css("display", "block");
 }
 
 function vanish (){
-    $("h3").css("display", "none");
-    $(".the_list").css("display", "none");
+    $("h3").text("Restante:");
+    $(".the_list").css("margin-bottom", "0");
     $(".button2").css("display", "none");
     $(".button").css("display", "none");
     $(".explain").css("display", "none");
@@ -39,12 +55,13 @@ function vanish (){
 
 function luck(){
     rnd = sort();
+    console.log(rnd);
     show(rnd);
-    vanish();   
+    vanish();
 }
 
 
-//Event
+//---------------------Event--------------
 
 $(".button").click(add);
 
@@ -59,7 +76,7 @@ $(".button2").click(
 });
 
 
-$(".button3").click(
+$(".button4").click(
     function(){
         location.reload();
 });
@@ -83,3 +100,8 @@ $(".doubt").click(
             verify = 0;
         }
 })
+
+$(".button3").click(
+    function(){
+        luck();
+    })
