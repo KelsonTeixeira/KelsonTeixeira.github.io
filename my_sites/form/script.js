cb0f480954e7f4887cbe4963ad562af649c83282
Kelson Teixeira <kelsonandre97@gmail.com>
2019-01-28 14:07:43 -0200

    deleted

--- my_sites/form/script.js
+++
@@ -1,9 +0,0 @@
-console.log("hello");
-
-var area_click = document.querySelectorAll(".inp");
-
-
-
-first.addEventListener ("click", function(){
-    area_click.style.borderBottom = "2px solid #000";
-})
