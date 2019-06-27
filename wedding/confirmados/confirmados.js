e102fec40f4913c6b55202458bf85060e8167174
Kelson Teixeira <kelsonandre97@gmail.com>
2019-06-27 20:55:46 -0300

    delete

--- wedding/confirmados/confirmados.js
+++
@@ -1,8 +0,0 @@
-const ul = document.getElementById('ul');
-firebase.database().ref('confirmados').on('value', function(snapshot){
-    snapshot.forEach(function(item){
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(item.val().name));
-        ul.appendChild(li);
-    });
-});
