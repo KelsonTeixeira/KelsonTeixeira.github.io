1f675c057a812944ca8d190f175b542b951b3a59
KelsonTeixeira <kelsonandre97@gmail.com>
2019-11-22 17:26:06 -0200

    new site part, portfolio add

---
+++ apps/embaixadores/README.md
@@ -0,0 +1,4 @@
+# embaixadores.github.io
+this is the site of Embaixadores do Reino
+
+Open the index.html file with your browser or access embaixadores.github.io 
---
+++ apps/js/apps.js
@@ -0,0 +1,44 @@
+const apps = [
+    {
+        name: 'Snake Game',
+        url: './snake',
+        git_url: 'https://github.com/KelsonTeixeira/KelsonTeixeira.github.io/tree/master/apps/snake',
+        description: 'Jogo da "Cobrinha", desenvolvido em JavaScript, utilizando os princípios OOP.',
+        image_url: './img/snake.jpg'
+    },
+    {
+        name: 'Sorteador',
+        url: './sort',
+        git_url: 'https://github.com/KelsonTeixeira/KelsonTeixeira.github.io/tree/master/apps/sort',
+        description: 'Sorteador personalizável, desenvolvido em HTML, CSS e JS. Adicione as opções e boa sorte.',
+        image_url: './img/sort.jpg'
+    },
+    {
+        name: 'Site Pessoal',
+        url: 'https://kelsonteixeira.github.io',
+        git_url: 'https://github.com/KelsonTeixeira/KelsonTeixeira.github.io',
+        description: 'Site pessoal, desenvolvido em HTML, CSS e JS. Apresentação rápida sobre mim.',
+        image_url: './img/personal.jpg'
+    },
+    {
+        name: 'Este Site',
+        url: './',
+        git_url: 'https://github.com/KelsonTeixeira/KelsonTeixeira.github.io/tree/master/apps',
+        description: 'Site desenvolvido para divulgação e organização dos meus projetos',
+        image_url: './img/apps.jpg'
+    },
+    {
+        name: 'Wedding Site',
+        url: './wedding',
+        git_url: 'https://github.com/KelsonTeixeira/KelsonTeixeira.github.io/tree/master/apps/wedding',
+        description: 'Site do meu casamento, desenvolvido por mim e minha esposa inspirado no wix.',
+        image_url: './img/wedding.jpg'
+    },
+    {
+        name: 'Embaixadores do Reino',
+        url: './embaixadores',
+        git_url: 'https://github.com/KelsonTeixeira/KelsonTeixeira.github.io/tree/master/apps/embaixadores',
+        description: 'Site desenvolvido para divulgação do retiro de uma igreja',
+        image_url: './img/embaixadores.jpg'
+    }
+]
