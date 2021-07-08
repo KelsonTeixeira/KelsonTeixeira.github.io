f1dd697699581ecdfe503f92aaf68b82b8748c9f
KelsonTeixeira <dev@kelsonteixeira.com>
2021-07-08 19:19:17 -0300

    portfolio updated

--- apps/js/apps.js
+++ apps/js/apps.js
@@ -1,44 +1,23 @@
 const apps = [
     {
+        name: 'Github Clone',
+        url: 'https://clone-github.netlify.app/',
+        git_url: 'https://github.com/KelsonTeixeira/githubclone',
+        description: 'Projeto pessoal usando ReactJS, clonando o Gitihub consumindo a API deste.',
+        image_url: './img/git-clone.png'
+    },
+    {
+        name: 'Solar Imóveis',
+        url: 'https://solarimoveis.net.br/',
+        git_url: '',
+        description: 'Projeto desenvolvido para Alpina Digital de um de Seus clientes. Usando Wordpress, JS, PHP, CSS e HTML',
+        image_url: './img/solar-imoveis.png'
+    },
+    {
         name: 'Snake Game',
         url: './snake',
         git_url: 'https://github.com/KelsonTeixeira/KelsonTeixeira.github.io/tree/master/apps/snake',
         description: 'Jogo da "Cobrinha", desenvolvido em JavaScript, utilizando os princípios OOP.',
         image_url: './img/snake.jpg'
-    },
-    {
-        name: 'Sorteador',
-        url: './sort',
-        git_url: 'https://github.com/KelsonTeixeira/KelsonTeixeira.github.io/tree/master/apps/sort',
-        description: 'Sorteador personalizável, desenvolvido em HTML, CSS e JS. Adicione as opções e boa sorte.',
-        image_url: './img/sort.jpg'
-    },
-    {
-        name: 'Site Pessoal',
-        url: 'https://kelsonteixeira.github.io',
-        git_url: 'https://github.com/KelsonTeixeira/KelsonTeixeira.github.io',
-        description: 'Site pessoal, desenvolvido em HTML, CSS e JS. Apresentação rápida sobre mim.',
-        image_url: './img/personal.jpg'
-    },
-    {
-        name: 'Este Site',
-        url: './',
-        git_url: 'https://github.com/KelsonTeixeira/KelsonTeixeira.github.io/tree/master/apps',
-        description: 'Site desenvolvido para divulgação e organização dos meus projetos',
-        image_url: './img/apps.jpg'
-    },
-    {
-        name: 'Wedding Site',
-        url: './wedding',
-        git_url: 'https://github.com/KelsonTeixeira/KelsonTeixeira.github.io/tree/master/apps/wedding',
-        description: 'Site do meu casamento, desenvolvido por mim e minha esposa inspirado no wix.',
-        image_url: './img/wedding.jpg'
-    },
-    {
-        name: 'Embaixadores do Reino',
-        url: './embaixadores',
-        git_url: 'https://github.com/KelsonTeixeira/KelsonTeixeira.github.io/tree/master/apps/embaixadores',
-        description: 'Site desenvolvido para divulgação do retiro de uma igreja',
-        image_url: './img/embaixadores.jpg'
     }
 ]
