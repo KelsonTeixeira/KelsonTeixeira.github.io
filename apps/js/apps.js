189f7f941a27c295302b60d79ce83169bae41eb5
KelsonTeixeira <dev@kelsonteixeira.com>
2021-07-08 19:31:37 -0300

    portfolio updated

--- apps/js/apps.js
+++ apps/js/apps.js
@@ -1,5 +1,19 @@
 const apps = [
     {
+        name: 'Alpina Digital',
+        url: 'https://alpina.digital/',
+        git_url: '',
+        description: 'Site da Alpina digital, desenvolvido enquanto freelancer. Usando Wordpress, JS, PHP, CSS e HTML',
+        image_url: './img/alpina.png'
+    },
+    {
+        name: 'Kumpel Contruções',
+        url: 'https://kumpelconstrucoes.com.br/',
+        git_url: '',
+        description: 'Projeto desenvolvido para Alpina Digital de um de seus clientes. Usando Wordpress, JS, PHP, CSS e HTML',
+        image_url: './img/kumpel.png'
+    },
+    {
         name: 'Github Clone',
         url: 'https://clone-github.netlify.app/',
         git_url: 'https://github.com/KelsonTeixeira/githubclone',
@@ -19,5 +33,12 @@ const apps = [
         git_url: 'https://github.com/KelsonTeixeira/KelsonTeixeira.github.io/tree/master/apps/snake',
         description: 'Jogo da "Cobrinha", desenvolvido em JavaScript, utilizando os princípios OOP.',
         image_url: './img/snake.jpg'
+    },
+    {
+        name: 'React Native Github Clone',
+        url: 'https://github.com/KelsonTeixeira/git-native',
+        git_url: '',
+        description: 'Projeto pessoal em React Native para entender um pouco mais sobre desenvolvimento mobile.',
+        image_url: './img/user.jpeg'
     }
 ]
